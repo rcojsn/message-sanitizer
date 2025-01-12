b6d2995d4dd7016a6ed252b48d56cec75f9f3f95
Ricardo Jansen <rcojsn@gmail.com>
2025-01-12 04:24:43 +0200

    update container name

--- backend/src/AdminService/AdminService.Infrastructure/Database/wait-and-run.sh
+++ backend/src/AdminService/AdminService.Infrastructure/Database/wait-and-run.sh
@@ -4,7 +4,7 @@
 echo "Waiting for SQL Server to be ready..."
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S database -U sa -P StrongP@ssw0rd123! -Q "SELECT 1" > /dev/null 2>&1
+    /opt/mssql-tools/bin/sqlcmd -S admin-database -U sa -P StrongP@ssw0rd123! -Q "SELECT 1" > /dev/null 2>&1
     if [ $? -eq 0 ]
     then
         echo "SQL Server is ready."
@@ -16,4 +16,4 @@ do
 done
 
 # Run the SQL script
-/opt/mssql-tools/bin/sqlcmd -S database -U sa -P StrongP@ssw0rd123! -d master -i /CreateDatabaseAndSeed.sql
+/opt/mssql-tools/bin/sqlcmd -S admin-database -U sa -P StrongP@ssw0rd123! -d master -i /CreateDatabaseAndSeed.sql
