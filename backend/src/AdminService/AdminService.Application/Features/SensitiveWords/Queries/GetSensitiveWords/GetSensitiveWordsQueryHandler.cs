24919022fdce139cd03029d2caa0081f00496bb8
Ricardo Jansen <rcojsn@gmail.com>
2025-01-16 00:57:58 +0200

    add logging for the get sensitive words handler

--- backend/src/AdminService/AdminService.Application/Features/SensitiveWords/Queries/GetSensitiveWords/GetSensitiveWordsQueryHandler.cs
+++ backend/src/AdminService/AdminService.Application/Features/SensitiveWords/Queries/GetSensitiveWords/GetSensitiveWordsQueryHandler.cs
@@ -2,15 +2,29 @@
 using AdminService.Application.Mappings;
 using BleepGuard.Contracts.SensitiveWords;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace AdminService.Application.Features.SensitiveWords.Queries.GetSensitiveWords;
 
-public class GetSensitiveWordsQueryHandler(ISensitiveWordsRepository sensitiveWordsRepository) : IRequestHandler<GetSensitiveWordsQuery, IList<SensitiveWordResponse>>
+public class GetSensitiveWordsQueryHandler(
+    ISensitiveWordsRepository sensitiveWordsRepository,
+    ILogger<GetSensitiveWordsQueryHandler> logger
+) : IRequestHandler<GetSensitiveWordsQuery, IList<SensitiveWordResponse>>
 {
     public async Task<IList<SensitiveWordResponse>> Handle(GetSensitiveWordsQuery request,
         CancellationToken cancellationToken)
     {
+        logger.LogInformation("Fetching all sensitive words");
+
         var sensitiveWords = await sensitiveWordsRepository.GetAllAsync();
+
+        if (!sensitiveWords.Any())
+        {
+            logger.LogWarning("No sensitive words found");
+            return [];
+        }
+
+        logger.LogInformation("Retrieved {Count} sensitive words", sensitiveWords.Count);
         return sensitiveWords.MapToSensitiveWordsResponse();
     }
 }
--- backend/tests/AdminService/AdminService.Application.Tests/Features/SensitiveWords/GetSensitiveWordsTests.cs
+++ backend/tests/AdminService/AdminService.Application.Tests/Features/SensitiveWords/GetSensitiveWordsTests.cs
@@ -2,8 +2,11 @@
 using AdminService.Application.Features.SensitiveWords.Queries.GetSensitiveWords;
 using AdminService.Application.Mappings;
 using AdminService.Domain.SensitiveWords;
+using Castle.Core.Logging;
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using NSubstitute;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace AdminService.Application.Tests.Features.SensitiveWords;
 
@@ -11,9 +14,10 @@ public class GetSensitiveWordsTests
 {
     private readonly GetSensitiveWordsQueryHandler _sut;
     private readonly ISensitiveWordsRepository _sensitiveWordsRepository = Substitute.For<ISensitiveWordsRepository>();
+    private readonly ILogger<GetSensitiveWordsQueryHandler> _logger = Substitute.For<ILogger<GetSensitiveWordsQueryHandler>>();
     
     public GetSensitiveWordsTests()
-    => _sut = new GetSensitiveWordsQueryHandler(_sensitiveWordsRepository);
+    => _sut = new GetSensitiveWordsQueryHandler(_sensitiveWordsRepository, _logger);
     
     [Fact]
     public async Task GetSensitiveWords_WhenCalled_ShouldReturnSensitiveWords()
